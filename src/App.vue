fe249933e598fc52c65685e3c63c48c81eb70781
2020807070 <2020807070@qq.com>
2018-06-16 16:48:24 +0800

    123

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <iframe style="width:100%;min-height:100%;background-color:#fff;" src="/weishi/feed/BpaXgQUDVtikTvPSCmGMHD/wsfeed?_proxy=1&amp;_wv=1&amp;id=BpaXgQUDVtikTvPSCmGMHD&amp;from=pc&amp;orifrom="></iframe>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
